b42aaf14da17c6de92a4cdc54f579c28761b1497
dewasanta2 <dewasanta2@gmail.com>
2022-06-12 12:24:21 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = ''
+MYSQL_DB = 'seal'
+
+SECRET_KEY = ''
+DEBUG = False
--- main.py
+++ main.py
@@ -3,30 +3,15 @@ from flask import (Flask, redirect, url_for)
 from flask_mysqldb import MySQL
 
 app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
 HTTP_PORT = 80
 
-# configure database mysql
-app.config['MYSQL_HOST'] = 'sib-seal-bali-sm2.cbjgmdgmfjls.us-west-1.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = 'sayalupa'
-app.config['MYSQL_DB'] = 'seal'
-
+# init mysql
 mysql = MySQL(app)
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
+def create_app():
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
@@ -46,4 +31,4 @@ def create_app(test_config=None):
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(port=HTTP_PORT, debug=True, host='0.0.0.0', use_reloader=True)
+    app.run(port=HTTP_PORT, host='0.0.0.0', use_reloader=True)
